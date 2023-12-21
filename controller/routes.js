9a623d9bed6f0f05dcfce329487b03773ea4ae55
daffairham <dafirham@gmail.com>
2023-12-21 18:55:27 +0700

    sudah bisa baca file csv dan masukkin ke database

--- controller/routes.js
+++ controller/routes.js
@@ -5,7 +5,8 @@ import path from 'path';
 import xlsx from "xlsx";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-
+import csv from 'csv-parser'
+import fs from 'fs';
 // components
 import { Header } from "../components/header/index.js"
 import { Navigation } from "../components/navigation-bar/index.js"
@@ -50,7 +51,7 @@ router.post("/upload", upload.single("uploadfile"), (req, res) => {
     if (!req.file) {
         return res.status(400).send("No file uploaded.");
     }
-    console.log(req.file); // Log file information
+    // console.log(req.file); // Log file information
     
     // Get the current module's file path
     const __filename = fileURLToPath(import.meta.url);
@@ -58,7 +59,7 @@ router.post("/upload", upload.single("uploadfile"), (req, res) => {
     const __dirname = dirname(__filename);
     
     const filePath = path.resolve("public", "uploads", req.file.filename);
-    importExcelData2MySQL(filePath)
+    importCSVData2MySQL(filePath)
         .then(() => {
             console.log("berhasil");
             res.redirect("/upload-csv");
@@ -69,118 +70,113 @@ router.post("/upload", upload.single("uploadfile"), (req, res) => {
         });
 });
 
+//FUNGSI PARSING FILE
+async function importCSVData2MySQL(filePath) {
+    try {
+        const rows = [];
+        
+        // Read CSV file and push rows to the array
+        await new Promise((resolve, reject) => {
+            fs.createReadStream(filePath)
+                .pipe(csv({ separator: ';' }))
+                .on('data', (row) => {
+                    rows.push(row);
+                    // console.log("Parsed Row:", row);
+                })
+                .on('end', resolve)
+                .on('error', reject);
+        });
 
-async function importExcelData2MySQL(filePath) {
+        // console.log("Rows:", rows);
+
+        const peopleData = [];
+        const productsData = [];
+        const promotionData = [];
+        const placeData = [];
+
+        rows.forEach((row) => {
+            const peopleRow = [
+                parseInt(row.Year_Birth) || null,
+                row.Education,
+                row.Marital_Status,
+                parseInt(row.Income) || null,
+                parseInt(row.Kidhome) || null,
+                parseInt(row.Teenhome) || null,
+                row.Dt_Customer,
+                parseInt(row.Recency) || null,
+                parseInt(row.Complain) || null,
+            ];
+
+            const productsRow = [
+                parseInt(row.MntWines) || null,
+                parseInt(row.MntFruits) || null,
+                parseInt(row.MntMeatProducts) || null,
+                parseInt(row.MntFishProducts) || null,
+                parseInt(row.MntSweetProducts) || null,
+                parseInt(row.MntGoldProds) || null,
+            ];
+            const promotionRow = [
+                parseInt(row.NumDealsPurchases) || null,
+                parseInt(row.AcceptedCmp1) || null,
+                parseInt(row.AcceptedCmp2) || null,
+                parseInt(row.AcceptedCmp3) || null,
+                parseInt(row.AcceptedCmp4) || null,
+                parseInt(row.AcceptedCmp5) || null,
+                parseInt(row.Response) || null,
+            ];
+            const placeRow = [
+                parseInt(row.NumWebPurchases) || null,
+                parseInt(row.NumCatalogPurchases) || null,
+                parseInt(row.NumStorePurchases) || null,
+                parseInt(row.NumWebVisitsMonth) || null,
+            ];
+
+            peopleData.push(peopleRow);
+            productsData.push(productsRow);
+            promotionData.push(promotionRow);
+            placeData.push(placeRow);
+        });
+
+        // Log the data before insertion
+        // console.log("People Data:", peopleData);
+        // console.log("Products Data:", productsData);
+        // console.log("Promotion Data:", promotionData);
+        // console.log("Place Data:", placeData);
+        console.log("ini coba", peopleData[0]);
+
+        // Define queries
+        const queries = [
+            { query: "INSERT INTO people (Year_Birth, Education, Marital_Status, Income, Kidhome, Teenhome, Dt_Customer, Recency, Complain) VALUES ?", data: peopleData },
+            { query: "INSERT INTO products (MntWines, MntFruits, MntMeatProducts, MntFishProducts, MntSweetProducts, MntGoldProds) VALUES ?", data: productsData },
+            { query: "INSERT INTO promotion (NumDealsPurchases, AcceptedCmp1, AcceptedCmp2, AcceptedCmp3, AcceptedCmp4, AcceptedCmp5, Response) VALUES ?", data: promotionData },
+            { query: "INSERT INTO place (NumWebPurchases, NumCatalogPurchases, NumStorePurchases, NumWebVisitsMonth) VALUES ?", data: placeData },
+        ];
+
+        // Execute queries using Promise.all
+        await Promise.all(queries.map(queryObject => executeQuery(queryObject)));
+
+        console.log("Data inserted into the database successfully");
+        return "Data inserted into the database successfully";
+    } catch (error) {
+        console.error("Error importing data:", error);
+        throw error;
+    }
+}
+
+function executeQuery({ query, data }) {
     return new Promise((resolve, reject) => {
-        readXlsxFile(filePath).then((rows) => {
-            console.log("Rows before shift:", rows);
-            rows.shift(); // Remove header row
-            console.log("Rows after shift:", rows);
-            const peopleData = [];
-            const productsData = [];
-            const promotionData = [];
-            const placeData = [];
-
-            rows.forEach((row) => {
-                // Assuming the Excel columns correspond to the table columns
-                const peopleRow = [
-                    parseInt(row.Year_Birth) || null,
-                    row.Education,
-                    row.Marital_Status,
-                    parseInt(row.Income) || null,
-                    parseInt(row.Kidhome) || null,
-                    parseInt(row.Teenhome) || null,
-                    row.Dt_Customer,
-                    parseInt(row.Recency) || null,
-                    parseInt(row.Complain) || null,
-                ];
-
-                const productsRow = [
-                    parseInt(row.MntWines) || null,
-                    parseInt(row.MntFruits) || null,
-                    parseInt(row.MntMeatProducts) || null,
-                    parseInt(row.MntFishProducts) || null,
-                    parseInt(row.MntSweetProducts) || null,
-                    parseInt(row.MntGoldProds) || null,
-                ];
-                const promotionRow = [
-                    parseInt(row.NumDealsPurchases) || null,
-                    parseInt(row.AcceptedCmp1) || null,
-                    parseInt(row.AcceptedCmp2) || null,
-                    parseInt(row.AcceptedCmp3) || null,
-                    parseInt(row.AcceptedCmp4) || null,
-                    parseInt(row.AcceptedCmp5) || null,
-                    parseInt(row.Response) || null,
-                ];
-                const placeRow = [
-                    parseInt(row.NumWebPurchases) || null,
-                    parseInt(row.NumCatalogPurchases) || null,
-                    parseInt(row.NumStorePurchases) || null,
-                    parseInt(row.NumWebVisitsMonth) || null,
-                ];
-
-                peopleData.push(peopleRow);
-                productsData.push(productsRow);
-                promotionData.push(promotionRow);
-                placeData.push(placeRow);
-            });
-            // Log the data before insertion
-            console.log("People Data:", peopleData);
-            console.log("Products Data:", productsData);
-            console.log("Promotion Data:", promotionData);
-            console.log("Place Data:", placeData);
-
-            // Insert data into People table
-            const peopleQuery =
-                "INSERT INTO people (Year_Birth, Education, Marital_Status, Income, Kidhome, Teenhome, Dt_Customer, Recency, Complain) VALUES ?";
-            pool.query(peopleQuery, [peopleData], (error, results) => {
-                if (error) {
-                    console.error("Error inserting into People table:", error);
-                    reject(error);
-                } else {
-                    console.log("Data inserted into People table successfully");
-                }
-            });
-
-            // Insert data into Products table
-            const productsQuery =
-                "INSERT INTO products (MntWines, MntFruits, MntMeatProducts, MntFishProducts, MntSweetProducts, MntGoldProds) VALUES ?";
-            pool.query(productsQuery, [productsData], (error, results) => {
-                if (error) {
-                    console.error("Error inserting into Products table:", error);
-                    reject(error);
-                } else {
-                    console.log("Data inserted into Products table successfully");
-                }
-            });
-
-            // Insert data into Promotion table
-            const promotionQuery =
-                "INSERT INTO promotion (NumDealsPurchases, AcceptedCmp1, AcceptedCmp2, AcceptedCmp3, AcceptedCmp4, AcceptedCmp5, Response) VALUES ?";
-            pool.query(promotionQuery, [promotionData], (error, results) => {
-                if (error) {
-                    console.error("Error inserting into Promotion table:", error);
-                    reject(error);
-                } else {
-                    console.log("Data inserted into Promotion table successfully");
-                }
-            });
-
-            // Insert data into Place table
-            const placeQuery =
-                "INSERT INTO place (NumWebPurchases, NumCatalogPurchases, NumStorePurchases, NumWebVisitsMonth) VALUES ?";
-            pool.query(placeQuery, [placeData], (error, results) => {
-                if (error) {
-                    console.error("Error inserting into Place table:", error);
-                    reject(error);
-                } else {
-                    console.log("Data inserted into Place table successfully");
-                    resolve("Data inserted into the database successfully");
-                }
-            });
+        pool.query(query, [data], (error, results) => {
+            if (error) {
+                console.error(`Error executing query: ${query}`, error);
+                reject(error);
+            } else {
+                console.log(`Query executed successfully: ${query}`);
+                resolve(results);
+            }
         });
     });
 }
+
 
 // ...
 
